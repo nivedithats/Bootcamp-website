d776c318ed34199a9a8cf8715e44e68ae44ed31a
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-01-12 19:14:58 +0530

    updated files

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -21,7 +21,7 @@ function Certificate() {
                 </p>
                 <h1 className='p-large-xl text-900 text-main mb-3'>Data Science developemnt Bootcamp:</h1>
                 <p className="p-large text-white">
-                The Bootcamp was conducted on  22<sup>nd</sup>-23 <sup>rd</sup> jan 2024, and its duration spanned  Day1 3Hours and day2 3Hours total 6 Hours Bootcamp, ensuring participants had ample time to acquire the skills and knowledge necessary to excel in the field of Data Science Developemnt.
+                The Bootcamp was conducted on  22<sup>nd</sup>-23 <sup>rd</sup> jan 2024, and its duration spanned  Day1 2Hours and day2 2Hours total 4 Hours Bootcamp, ensuring participants had ample time to acquire the skills and knowledge necessary to excel in the field of Data Science Developemnt.
                 </p>
             </div>
         </div>
