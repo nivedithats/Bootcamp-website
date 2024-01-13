132e9e02178881243f44dfd1d98feda8b328bd35
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-01-13 18:21:16 +0530

    updated files

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,7 @@ function Works() {
         <div className="container">
             {/* <h1 className=" text-center text-white mb-5">How Our Data Science Bootcamp Works Over <span className="text-main">6 hours</span></h1> */}
             <div className="work-text-container p-3 p-lg-5">
-            <h1 className="heading-subtitle text-white mb-3"> <i class="bi bi-star text-main mx-2"></i>6 Hours Bootcamp</h1>
+            <h1 className="heading-subtitle text-white mb-3"> <i class="bi bi-star text-main mx-2"></i>4 Hours Bootcamp</h1>
             {
                 works.working.map((item)=>(
                     <p className='p-large'><i class="bi bi-check2 text-main"></i> {item.works}</p>
